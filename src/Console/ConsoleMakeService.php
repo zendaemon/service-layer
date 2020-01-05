727fba21bb1f2744f782ff24aba8c8aed64b1b92
dev <olenin.denis@gmail.com>
2020-01-06 00:43:48 +0200

    Fixed code style

--- src/Console/ConsoleMakeService.php
+++ src/Console/ConsoleMakeService.php
@@ -51,7 +51,7 @@ class ConsoleMakeService extends GeneratorCommand
      * Get the default namespace for the class.
      *
      * @param string $rootNamespace
-     * 
+     *
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
