3bbca7ef753d24c38b6d0b6f7ca00f819f799ad5
Dev <olenin.denis@gmail.com>
2021-02-23 17:19:05 +0200

    Fixed code style

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 
 class InstallCommand extends Command
 {
-    private const PROVIDER_NAME = "ServiceLayerServiceProvider";
+    private const PROVIDER_NAME = 'ServiceLayerServiceProvider';
 
     /**
      * The name and signature of the console command.
@@ -50,23 +50,23 @@ class InstallCommand extends Command
 
         $appConfig = file_get_contents(config_path('app.php'));
 
-        if (Str::contains($appConfig, $namespace . '\\Providers\\' . self::PROVIDER_NAME . '::class')) {
+        if (Str::contains($appConfig, $namespace . '\\Providers\\'.self::PROVIDER_NAME.'::class')) {
             return;
         }
 
-        if (!file_exists(app_path('Providers/' . self::PROVIDER_NAME . '.php'))) {
-            throw new \Exception(self::PROVIDER_NAME . ' not published.');
+        if (!file_exists(app_path('Providers/'.self::PROVIDER_NAME.'.php'))) {
+            throw new \Exception(self::PROVIDER_NAME.' not published.');
         }
 
-        file_put_contents(app_path('Providers/' . self::PROVIDER_NAME . '.php'), str_replace(
+        file_put_contents(app_path('Providers/'.self::PROVIDER_NAME.'.php'), str_replace(
             "namespace App\Providers;",
             "namespace {$namespace}\Providers;",
-            file_get_contents(app_path('Providers/' . self::PROVIDER_NAME . '.php'))
+            file_get_contents(app_path('Providers/'.self::PROVIDER_NAME.'.php'))
         ));
 
         file_put_contents(config_path('app.php'), str_replace(
             "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL,
-            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL."        {$namespace}\Providers\\" . self::PROVIDER_NAME . "::class,".PHP_EOL,
+            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL."        {$namespace}\Providers\\".self::PROVIDER_NAME."::class,".PHP_EOL,
             $appConfig
         ));
     }
